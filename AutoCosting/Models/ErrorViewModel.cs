d6259f282f7173c828f95b228d4d44f4a7fb5232
Jose Pablo Montero Soto <jpmontero-@hotmail.com>
2018-09-23 16:47:12 -0600

    Crear la plantilla del proyecto

---
+++ AutoCosting/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AutoCosting.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
