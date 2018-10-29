6091332f0756d6dfb1e297633b462c2033e28028
Jose Pablo Montero Soto <jpmontero-@hotmail.com>
2018-10-28 18:40:11 -0600

    Simple financing calculator

---
+++ AutoCosting/Controllers/CalcFinanciamiento/CalcFinanciamientoController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoCosting.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AutoCosting.Models;
+
+namespace AutoCosting.Controllers.CalcFinanciamiento
+{
+    public class CalcFinanciamientoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CalcFinanciamientoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(AutoCosting.Models.Calc.CalcFinanciamiento calc)
+        {
+
+            ViewData["VINVehiculo"] = new SelectList(_context.Vehiculos, "VIN", "Descripcion");
+            calc.Calcular();
+            return this.RedirectToAction(nameof(Resultado), calc);
+        }       
+
+        public IActionResult Index()
+        {
+            ViewData["VINVehiculo"] = new SelectList(_context.Vehiculos, "VIN", "Descripcion");
+            return View();
+        }
+
+        public IActionResult Resultado(AutoCosting.Models.Calc.CalcFinanciamiento calc)
+        {
+            ViewData["VINVehiculo"] = new SelectList(_context.Vehiculos, "VIN", "Descripcion");
+            return View(calc);
+        }
+
+        [HttpPost]
+        public string GetPrecioVehiculo(string vin)
+        {
+            var vehiculo = this._context.Vehiculos.FirstOrDefault(v => v.VIN == vin);
+            if (vehiculo != null)
+            {
+                return vehiculo.PrecioRecomendado.ToString();
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ AutoCosting/Models/Calc/CalcFinanciamiento.cs
@@ -0,0 +1,47 @@
+﻿using AutoCosting.HelpersAndValidations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoCosting.Models.Calc
+{
+    public class CalcFinanciamiento
+    {
+        [Required(ErrorMessage = "El vehículo es requerido.")]
+        public string Vehiculo { get; set; }
+        [Required(ErrorMessage = "El precio es requerido.")]
+        public double? Precio { get; set; }
+        [Display(Name = "Tasa de Interés Anual (%)")]
+        [Required(ErrorMessage = "El porcentaje de interés es requerido.")]
+        public double? Interes { get; set; }
+        [Required(ErrorMessage = "La cantidad de cuotas es requerida.")]
+        public int? NumeroCuotas { get; set; }
+        [Display(Name = "Tarifa Adicional.")]
+        public double? TarifaAddicional { get; set; }        
+        public double? Prima { get; set; }
+        public double? Descuento { get; set; }
+        public string Resultado { get; set; }
+
+        public void Calcular()
+        {
+            double precioTotal = this.Precio.GetValueOrDefault(0);
+            double cuota = 0;
+
+            precioTotal += this.TarifaAddicional.GetValueOrDefault(0);
+            precioTotal -= this.Descuento.GetValueOrDefault(0);
+            precioTotal -= this.Prima.GetValueOrDefault(0);
+            //A = P*(r(1+r)^n)/((1+r)^n -1)
+            //P = Capital Principal
+            //A = Pago Mensual
+            //r = Tasa de Interes
+            //n = Total de Meses
+            double r = (Interes.GetValueOrDefault(0)/100) / 12;
+            double n = this.NumeroCuotas.GetValueOrDefault(0);
+            cuota = precioTotal * ((r * Math.Pow( 1 + r, n)) / 
+                                    (Math.Pow(1+r,n) - 1));
+            Resultado = "La cuota Mensual aproximada es de: " + cuota.ToString("#,###.00");
+        }
+    }
+}
