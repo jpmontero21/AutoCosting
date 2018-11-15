a41b02cea7d139ca45a3521784ba9d0bc58bb162
Jose Pablo Montero Soto <jpmontero-@hotmail.com>
2018-11-14 19:02:16 -0600

    DBBackup controller and Model

---
+++ AutoCosting/Models/Backups/DbBackup.cs
@@ -0,0 +1,20 @@
+﻿using AutoCosting.Models.Maintenance;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoCosting.Models.Backups
+{
+    public class DbBackup
+    {
+        [Display(Name = "Ruta")]
+        public string Path { get; set; }
+        [Display(Name = "Nombre del archivo")]
+        public string Name { get; set; }
+        public Empresa Empresa { get; set; }
+        [Display(Name= "Archivo de Respaldo")]
+        public string RestoreFilePath { get; set; }
+    }
+}
